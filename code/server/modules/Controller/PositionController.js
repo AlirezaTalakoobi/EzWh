1171b4f6de02568482975f418800fba440dbaa34
Talakoobi <s289641@studenti.polito.it>
2022-05-18 00:47:58 +0200

    finished position withour exceptions

--- code/server/modules/Controller/PositionController.js
+++ code/server/modules/Controller/PositionController.js
@@ -33,24 +33,53 @@ class PositionController {
   createPosition = async (req, res) => {
 
     const ApiInfo = req.body;
-    const sql = `INSERT INTO POSITION (ID,aislelID, ROW, COL, MAXWEIGHT,MAXVOLUME, OCCUPIEDWEIGHT, OCCUPIEDWEIGHT) VALUES(?,?,?,?,?,?,?,?) `;
+    const sql = `INSERT INTO POSITION (ID,aisleID, ROW, COL, MAXWEIGHT,MAXVOLUME, OCCUPIEDWEIGHT, OCCUPIEDVOLUME) VALUES(?,?,?,?,?,?,?,?) `;
     const args = [ApiInfo.positionID, ApiInfo.aisleID, ApiInfo.row, ApiInfo.col, ApiInfo.maxWeight, ApiInfo.maxVolume, 0, 0];
     let result = await this.dao.run(sql, args);
     res.status(201);
 
   };
   modifyPosition =async (req, res) => {
+    const ApiInfo = req.body;
+    const sql = "UPDATE POSITION SET aisleID = ?, ROW = ?, COL = ?, MAXWEIGHT = ?, MAXVOLUME = ?, OCCUPIEDWEIGHT = ?, OCCUPIEDVOLUME = ?  WHERE ID = ? ";
+    const args = [ApiInfo.newAisleID, ApiInfo.newRow, ApiInfo.newCol,ApiInfo.newMaxWeight, ApiInfo.newMaxVolume, ApiInfo.newOccupiedWeight, ApiInfo.newOccupiedVolume, req.params.positionID];
+    try{
+        let result = await this.dao.run(sql, args);
+        res.sendStatus(200);
 
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
   };
 
   changePositionID = async (req, res) => {
+    const ApiInfo = req.body;
+    const sql = "UPDATE POSITION SET ID = ? WHERE ID = ? ";
+    const args = [ApiInfo.newPositionID, req.params.positionID];
+    try{
+        let result = await this.dao.run(sql, args);
+        res.sendStatus(200);
 
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
   };
 
 
   deletePosition = async (req, res) => {
+    const sql = "DELETE FROM POSITION WHERE ID = ? ";
+    const args = [req.params.positionID];
+    try{
+        let result = await this.dao.run(sql, args);
+        res.sendStatus(204);
 
-  };
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+};
   
   
 }
