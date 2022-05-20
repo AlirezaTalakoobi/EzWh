aba7c6c7d3b2d327aa6b31fa54ec72dc56305722
Pouya Hakimi Fard S296734 <S296734@studenti.polito.it>
2022-05-20 14:02:34 +0200

    validation Part

--- code/server/modules/Routes/SkuRouter.js
+++ code/server/modules/Routes/SkuRouter.js
@@ -5,14 +5,66 @@ const router = express.Router();
 const SKUController = require("../Controller/SKUController");
 const DAO = require("../DB/DAO");
 const dao = new DAO();
-console.log("test");
 const su = new SKUController(dao);
+const { check, param, validationResult} = require("express-validator");
 
 router.get("/skus", su.getsku);
-router.get("/skus/:id", su.getSKUbyId);
-router.post("/sku/", su.newSKU);
-router.put("/sku/:id", su.editsku);
-router.put("/sku/:id/position", su.editskuPosition);
-router.delete("/skus/:id", su.deleteSKU);
+router.get("/skus/:id", [param("id").isNumeric()],
+(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+}, su.getSKUbyId);
+
+router.post("/sku/", [
+    check("description").isString().isLength({ min: 1, max: 32 }),
+    check("weight").isNumeric(),
+    check("volume").isNumeric(),
+    check("notes").isString(),
+    check("price").isNumeric(),
+    check("availableQuantity").isNumeric()
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },su.newSKU);
+router.put("/sku/:id", [
+    param("id").isString().isLength({ min: 1, max: 32 }).not().optional(),
+    check("newWeight").isNumeric().optional(),
+    check("newVolume").isNumeric().optional(),
+    check("newNotes").isString().optional(),
+    check("newPrice").isNumeric().optional(),
+    check("newAvailableQuantity").isNumeric().optional()
+],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  }, su.editsku);
+  
+router.put("/sku/:id/position", [param("id").isNumeric().not().optional(),
+check("position").isString().not().optional()],
+(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+},su.editskuPosition);
+router.delete("/skus/:id", [param("id").isNumeric().not().optional()],
+(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+}, su.deleteSKU);
 
 module.exports = router;
--- code/server/modules/Routes/TDRouter.js
+++ code/server/modules/Routes/TDRouter.js
@@ -6,11 +6,48 @@ const TDController = require("../Controller/TDController");
 const DAO = require("../DB/DAO");
 const dao = new DAO();
 const td = new TDController(dao);
+const { check, param, validationResult} = require("express-validator");
 
 router.get("/testdescriptors", td.TestDescriptor);
-router.get("/testdescriptors/:id", td.getTestDescriptionById);
-router.post("/testdescriptor", td.newTestDescriptor);
-router.put("/testdescriptor/:id", td.editTDbyid);
-router.delete("/testdescriptor/:id", td.deleteTD);
+router.get("/testdescriptors/:id",[param("id").isNumeric().not().optional()],
+(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+}, td.getTestDescriptionById);
+router.post("/testdescriptor",[
+    check("name").isString().isLength({ min: 1, max: 32 }),
+    check("procedureDescription").isString(),
+    check("idSKU").isNumeric()
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  }, td.newTestDescriptor);
+router.put("/testdescriptor/:id", [
+    check("newName").isString().isLength({ min: 1, max: 32 }).optional(),
+    check("newProcedureDescription").isString().optional(),
+    check("newIdSKU").isNumeric().optional()
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },td.editTDbyid);
+router.delete("/testdescriptor/:id",  [param("id").isNumeric().not().optional()],
+(req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+},td.deleteTD);
 
 module.exports = router;
