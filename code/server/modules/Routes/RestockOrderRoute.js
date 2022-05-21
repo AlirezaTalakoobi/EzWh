ee4634e3e22d0ae371b45289ffbc357b401af519
Davide Andriano s302094 <s302094@studenti.polito.it>
2022-05-21 11:16:03 +0200

    Fixed internal/restock/return orders

--- code/server/modules/Routes/RestockOrderRoute.js
+++ code/server/modules/Routes/RestockOrderRoute.js
@@ -7,100 +7,115 @@ const dao = new DAO();
 const sic = new RestockOrderController(dao);
 const { check, param, validationResult } = require("express-validator");
 
-
 router.get("/restockOrders", sic.getRestockOrders);
 
-router.get("/restockOrder/:id",
-    [param("id").isNumeric()],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array()});
-        }
-        next();
-    },
-    sic.getRestockOrder
+router.get(
+  "/restockOrder/:id",
+  [param("id").isNumeric()],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.getRestockOrder
 );
 
 router.get("/restockOrdersIssued", sic.getRestockOrdersIssued);
 
-router.post("/restockOrder",
-    [
-        check("supplierId").isNumeric(),
-        check("issueDate").isDate(),
-        check("products").isArray(),
-        check("products.*.SKUId").isNumeric(),
-        check("products.*.description").isString(),
-        check("products.*.price").isNumeric(),
-        check("products.*.qty").isNumeric(),
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },
-    sic.createRestockOrder
+router.post(
+  "/restockOrder",
+  [
+    check("supplierId").isNumeric(),
+    check("issueDate").isDate(),
+    check("products").isArray(),
+    check("products.*.SKUId").isNumeric(),
+    check("products.*.description").isString(),
+    check("products.*.price").isNumeric(),
+    check("products.*.qty").isNumeric(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.createRestockOrder
 );
 
-router.put("/restockOrder/:id",
-    [
-        param("id").isNumeric(),
-        check("newState").isString().isIn(["ISSUED", "DELIVERY", "DELIVERED", "TESTED", "COMPLETEDRETURN", "COMPLETED"])
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },
-    sic.changeStateOfRestockOrder
+router.put(
+  "/restockOrder/:id",
+  [
+    param("id").isNumeric(),
+    check("newState")
+      .isString()
+      .isIn([
+        "ISSUED",
+        "DELIVERY",
+        "DELIVERED",
+        "TESTED",
+        "COMPLETEDRETURN",
+        "COMPLETED",
+      ]),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.changeStateOfRestockOrder
 );
 
-router.put("/restockOrder/:id/skuItems",
-    [
-        param("id").isNumeric(),
-        check("skuItems").isArray(),
-        check("skuItems.*.rfid").isString().isLength({min: 32, max: 32}),
-        check("skuItems.*.SKUId").isNumeric()
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },
-    sic.addSkuItemsToRestockOrder
+router.put(
+  "/restockOrder/:id/skuItems",
+  [
+    param("id").isNumeric(),
+    check("skuItems").isArray(),
+    check("skuItems.*.rfid").isString().isLength({ min: 32, max: 32 }),
+    check("skuItems.*.SKUId").isNumeric(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.addSkuItemsToRestockOrder
 );
 
-router.put("/restockOrder/:id/transportNote",
-    [
-        param("id").isNumeric(),
-        check("transportNote").isObject(),
-        check("transportNote.deliveryDate").isDate()
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },
-    sic.addTransportNoteToRestockOrder
+router.put(
+  "/restockOrder/:id/transportNote",
+  [
+    param("id").isNumeric(),
+    check("transportNote").isObject(),
+    check("transportNote.deliveryDate").isDate(),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.addTransportNoteToRestockOrder
 );
 
-router.delete("restockOrder/:id", 
-    [param("id").isNumeric()],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },sic.deleteRestockOrder
+router.delete(
+  "/restockOrder/:id",
+  [param("id").isNumeric()],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.deleteRestockOrder
 );
 
 module.exports = router;
--- code/server/modules/Routes/ReturnOrderRoute.js
+++ code/server/modules/Routes/ReturnOrderRoute.js
@@ -7,51 +7,53 @@ const dao = new DAO();
 const sic = new ReturnOrderController(dao);
 const { check, param, validationResult } = require("express-validator");
 
-
 router.get("/returnOrders", sic.getReturnOrders);
 
-router.get("/returnOrder/:id",
-    [param("id").isNumeric()],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },
-    sic.getReturnOrder
+router.get(
+  "/returnOrder/:id",
+  [param("id").isNumeric()],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.getReturnOrder
 );
 
-router.post("/returnOrder",
-    [
-        check("returnDate").isDate(),
-        check("restockOrderId").isNumeric(),
-        check("products").isArray(),
-        check("products.*.SKUId").isNumeric(),
-        check("products.*.description").isString(),
-        check("products.*.price").isNumeric(),
-        check("products.*.RFID").isString().isLength({min: 32, max: 32})
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },
-    sic.createReturnOrder
+router.post(
+  "/returnOrder",
+  [
+    check("returnDate").isDate(),
+    check("restockOrderId").isNumeric(),
+    check("products").isArray(),
+    check("products.*.SKUId").isNumeric(),
+    check("products.*.description").isString(),
+    check("products.*.price").isNumeric(),
+    check("products.*.RFID").isString().isLength({ min: 32, max: 32 }),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.createReturnOrder
 );
 
-router.delete("internalOrder/:id",
-    [param("id").isNumeric()],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        }
-        next();
-    },
-    sic.deleteReturnOrder
+router.delete(
+  "/returnOrder/:id",
+  [param("id").isNumeric()],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  },
+  sic.deleteReturnOrder
 );
 
 module.exports = router;
