d1332cf092fd02a78f9a9ce172bb6b2039ab5b9d
Davide Andriano s302094 <s302094@studenti.polito.it>
2022-06-03 19:07:59 +0200

    Fixed Items

--- code/server/modules/Controller/ItemController.js
+++ code/server/modules/Controller/ItemController.js
@@ -7,67 +7,65 @@ class ItemController {
   }
 
   getItems = async () => {
-
     try {
       const sql = "select * from ITEM";
       let result = await this.dao.all(sql, []);
-        result = result.map((rows) => ({
-          id: rows.ID,
-          description: rows.description,
-          price: rows.price,
-          SKUId: rows.skuID,
-          supplierId: rows.supplierID,
-        }));
-        let ret = {
-          ans : 200,
-          result : result
-        }
-        return ret;
+      result = result.map((rows) => ({
+        id: rows.ID,
+        description: rows.description,
+        price: rows.price,
+        SKUId: rows.skuID,
+        supplierId: rows.supplierID,
+      }));
+      let ret = {
+        ans: 200,
+        result: result,
+      };
+      return ret;
     } catch (err) {
       let ret = {
-        ans : 500,
-        result : {}
-      }
-     return ret;
+        ans: 500,
+        result: {},
+      };
+      return ret;
     }
   };
 
   getItemByID = async (id) => {
-    
     try {
       const sql = "select * from ITEM where ID=? ";
       const args = [id];
       let result = await this.dao.all(sql, args);
       if (result.length === 0) {
         let ret = {
-          ans : 404,
-          result : {}
-        }
-        return ret;
-      }
-        result = result.map((rows) => ({
-          id: rows.ID,
-          description: rows.description,
-          price: rows.price,
-          SKUId: rows.skuID,
-          supplierId: rows.supplierID,
-        }));
-        let ret = {
-          ans : 200,
-          result : result
+          ans: 404,
+          result: {},
         };
         return ret;
-    } catch (err) {
-      console.log(err)
+      }
+      result = result.map((rows) => ({
+        id: rows.ID,
+        description: rows.description,
+        price: rows.price,
+        SKUId: rows.skuID,
+        supplierId: rows.supplierID,
+      }));
       let ret = {
-        ans : 500,
-        result : {}
+        ans: 200,
+        result: result[0],
+      };
+      return ret;
+    } catch (err) {
+      console.log(err);
+      let ret = {
+        ans: 500,
+        result: {},
       };
       return ret;
     }
   };
 
-  createItem = async (id,description,price,SKUId,supplierId) => {
+  createItem = async (id, description, price, SKUId, supplierId) => {
     try {
       const sql_c_1 = "SELECT * FROM USER WHERE ID = ? ";
       const args_c_1 = [supplierId];
@@ -79,35 +77,25 @@ class ItemController {
       const args_c_2 = [SKUId];
       let check2 = await this.dao.all(sql_c_2, args_c_2);
       if (check2.length === 0) {
-
-        return 404
+        return 404;
       }
 
       const sql = `INSERT INTO ITEM (ID,description, price, skuId, supplierId) VALUES(?,?,?,?,?) `;
-      const args = [
-        id,
-        description,
-        price,
-        SKUId,
-        supplierId
-      ];
+      const args = [id, description, price, SKUId, supplierId];
       let result = await this.dao.run(sql, args);
       return 201;
     } catch (err) {
-
-      console.log(err)
+      console.log(err);
       return 503;
     }
   };
 
-  modifyItem = async (id,newPrice,newDescription) => {
+  modifyItem = async (id, newPrice, newDescription) => {
     try {
-      
       const sql_c_1 = "SELECT * FROM ITEM WHERE ID= ?";
       const args_c_1 = [id];
       let check1 = await this.dao.all(sql_c_1, args_c_1);
       if (check1.length === 0) {
-
         return 404;
       }
 
@@ -121,8 +109,6 @@ class ItemController {
   };
 
   deleteItem = async (id) => {
-    
-    
     try {
       const sql = "DELETE FROM ITEM WHERE ID = ? ";
       const args = [id];
@@ -133,15 +119,12 @@ class ItemController {
     }
   };
 
-
   deleteAll = async () => {
-    
     const sql = "DELETE FROM ITEM";
-    try{
-        let result = await this.dao.run(sql, []);
-        return true;
-    }
-    catch(err){
+    try {
+      let result = await this.dao.run(sql, []);
+      return true;
+    } catch (err) {
       return false;
     }
   };
